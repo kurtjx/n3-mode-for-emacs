f20dfa2684f601e1c46f115980c7f4c7af4c82fb
kurtjx <kurtjx@gmail.com>
2010-10-07 12:24:02 -0500

    added comment/uncomment block with M-;

--- n3-mode.el
+++ n3-mode.el
@@ -1,7 +1,7 @@
 ;;; n3-mode.el --- mode for Notation 3
-;   $Id: n3-mode.el 4084 2007-12-15 17:10:13Z hugoh $
 
 ;; Copyright (c) 2003-2007 Hugo Haas <hugo@larve.net>
+;; re-worked and re-published by kurtjx (c) 2010 <kurtjx@gmail.com>
 
 ;; For documentation on Notation 3, see:
 ;; http://www.w3.org/DesignIssues/Notation3.html
@@ -15,30 +15,46 @@
 
 ;; What it does now:
 ;; - Syntax highlighting
+;; - comment/uncomment block with M-;
 
 ;;; Code:
 
-(require 'generic)
+;; the command to comment/uncomment text
+(defun n3-comment-dwim (arg)
+"Comment or uncomment current line or region in a smart way.
+For detail, see `comment-dwim'."
+   (interactive "*P")
+   (require 'newcomment)
+   (let ((deactivate-mark nil) (comment-start "#") (comment-end ""))
+     (comment-dwim arg)))
 
-(define-generic-mode 'n3-mode
-  ;; comment char
-  (list "# ")
-  ;; keywords
-  (list "this" "a")
-  ;; additional font-lock'ing
+(setq n3-highlights
   '(("\\(@prefix\\)\\>" 1 font-lock-keyword-face t)
+    ("\\(a\\)\\>" 1 font-lock-keyword-face t)
     ("\\(\\S-*?:\\)" 1 font-lock-type-face t)
-    (":\\(.*?\\)[ ;.]" 1 font-lock-constant-face t)
+    (":\\(.+?\\)[ ;.]" 1 font-lock-constant-face t)
     ("\\(<.*?>\\)" 1 font-lock-function-name-face t)
     ("\\(\\\".*?\\\"\\)" 1 font-lock-string-face t)
-; Bug: some trailing characters are highlighted; restricting comments regexp
-;    ("\\(#.*\\)" 1 font-lock-comment-face t)
+    ; Bug: some trailing characters are highlighted; restricting comments regexp
+    ; ("\\(#.*\\)" 1 font-lock-comment-face t)
     ("^\\s-*\\(#.*\\)" 1 font-lock-comment-face t)
     )
-  ;; auto-mode
-  (list "\\.n3$")
-  ;; additional setup
-  nil
+)
+
+;;(define-generic-mode 'n3-mode
+(define-derived-mode n3-mode fundamental-mode
+  ;; setup tab key not working :/
+  ;;(setq c-basic-offset 4)
+
+  ;; syntax highlighting
+  (setq font-lock-defaults '(n3-highlights))
+
+  ;; modify the keymap M-; comments/uncomments region
+  (define-key n3-mode-map [remap comment-dwim] 'n3-comment-dwim)
+  ;; comments: “# ...” 
+  (modify-syntax-entry ?# "< b" n3-mode-syntax-table)
+  (modify-syntax-entry ?\n "> b" n3-mode-syntax-table)
+
   ;; description
   "Mode for Notation 3 documents."
-  )
+)
